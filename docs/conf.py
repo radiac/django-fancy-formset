7cdf5262c2dcdb7a47df58b517fa99d99baa77fd
Richard Terry <git@radiac.net>
2023-07-26 23:21:21 +0100

    Fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,9 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import re
+from pathlib import Path
+
 import sphinx_radiac_theme  # noqa
 
 
